19110ef15f808541aa8739d248cdd027a6159d03
Dionne Gonzalez <dionne.gonzalez.glez@gmail.com>
2018-12-12 15:13:59 -0500

    remove stuff

--- clickygame/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-
-import Header from './components/Header.css'
-
-ReactDOM.render(<Header />, document.getElementById('root'));
-
